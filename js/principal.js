9b4ae59b831769e4f5102a8b9aeda00fb38cd337
Gabriel Santos <Gsantoos94@gmail.com>
2022-07-26 22:23:15 -0300

    Criação do formulário e atualização da tabela
    Foi adicionado um formulário para a inserção dos dados de um novo paciente e atualização da tabela com os mesmos. Além disso, o background do "td" mudará caso haja um dado inválido.

--- js/principal.js
+++ js/principal.js
@@ -2,15 +2,66 @@ var titulo = document.querySelector(".titulo"); //busca na página o seletor esp
 
 titulo.textContent = "Aparecida Nutricionista"; // Altera o conteudo de texto do elemento
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for(i = 0; i < pacientes.length; i++){
 
-var imc = peso / (altura * altura);
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
 
-tdImc.textContent = imc;
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    if (peso <= 0 || peso >= 1000){
+        tdImc.textContent = "Peso inválido!";
+        console.log("Peso inválido!");
+        paciente.classList.add("paciente-invalido");
+    } else if (altura <= 0 || altura >= 3){
+        tdImc.textContent = "Altura Inválida!";
+        console.log("Altura Inválida");
+        paciente.classList.add("paciente-invalido");
+    } else{
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+ 
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    tabela.appendChild(pacienteTr);
+    
+})
